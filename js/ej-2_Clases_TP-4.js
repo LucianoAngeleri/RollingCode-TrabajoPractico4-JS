e0ea8e9f5841f3723ae8a88006240707af0f883a
Luciano Angeleri <lcnang@gmail.com>
2023-03-26 16:26:59 -0300

    Se realizaron cambios en los metodos de ingresar y extraer

--- js/ej-2_Clases_TP-4.js
+++ js/ej-2_Clases_TP-4.js
@@ -10,28 +10,28 @@ Escribe un programa que cree un objeto "cuenta" con las siguientes propiedades:
 Utiliza este objeto para mostrar la descripción, ingresar y extraer dinero y volver a mostrar la descripción del estado de la cuenta.
 */
 let cuenta = {
-    titular : "Alex",
-    saldo : 0,
-    ingresar : function (cantidad){
-        this.saldo = this.saldo + cantidad
-    },
-    extraer : function (cantidad){
-        this.saldo = this.saldo - cantidad
-    },
-    informar : function (){
-        return `El saldo de la cuenta de ${this.titular} es : $${this.saldo}`
-    }
-}
-document.write('<section class="container">')
-document.write('<h5 class="display-5 text-center text-success">Propiedades del objeto "cuenta" creado:</h5>')
-document.write('<ul class="list-group">')
-document.write(`<li class="list-group-item">Titular de la cuenta: ${cuenta.titular}</li>`)
-document.write(`<li class="list-group-item">Saldo disponible de la cuenta: ${cuenta.saldo}</li>`)
-document.write('<h5 class="display-5 text-center text-success">Uso de los métodos del objeto "cuenta":</h5>')
-document.write(`<p class="fs-3 text-success">Añadimos $200 a la cuenta con ingresar()</p>`)
+  titular: "Alex",
+  saldo: 0,
+  ingresar: function (cantidad) {
+    this.saldo += cantidad;
+  },
+  extraer: function (cantidad) {
+    this.saldo -= cantidad;
+  },
+  informar: function () {
+    return `El saldo de la cuenta de ${this.titular} es : $${this.saldo}`;
+  },
+};
+document.write('<section class="container">');
+document.write('<h5 class="display-5 text-center text-success">Propiedades del objeto "cuenta" creado:</h5>');
+document.write('<ul class="list-group">');
+document.write(`<li class="list-group-item">Titular de la cuenta: ${cuenta.titular}</li>`);
+document.write(`<li class="list-group-item">Saldo disponible de la cuenta: ${cuenta.saldo}</li>`);
+document.write('<h5 class="display-5 text-center text-success">Uso de los métodos del objeto "cuenta":</h5>');
+document.write(`<p class="fs-3 text-success">Añadimos $200 a la cuenta con ingresar()</p>`);
 cuenta.ingresar(200);
-document.write(`<p class="fs-3 text-secondary">${cuenta.informar()}</p>`)
-document.write(`<p class="fs-3 text-danger">Extraemos $50 a la cuenta con extraer()</p>`)
+document.write(`<p class="fs-3 text-secondary">${cuenta.informar()}</p>`);
+document.write(`<p class="fs-3 text-danger">Extraemos $50 a la cuenta con extraer()</p>`);
 cuenta.extraer(50);
-document.write(`<p class="fs-3 text-secondary">${cuenta.informar()}</p>`)
-document.write('</section>')
+document.write(`<p class="fs-3 text-secondary">${cuenta.informar()}</p>`);
+document.write("</section>");
