f0830250528e59b0ef34e3eb265f24b2a10e6bc3
Luciano Angeleri <lcnang@gmail.com>
2023-03-26 16:33:20 -0300

    Se planteo el objeto producto y el metodo para mostrar las propiedades

--- js/ej-4_Clases_TP-4.js
+++ js/ej-4_Clases_TP-4.js
@@ -1,8 +1,16 @@
 /*
 Ejercicio 4 - Producto - Modelando Clases
-Escribe una clase Producto para crear objetos. Estos objetos, deben presentar las propiedades código, nombre y precio, además del método imprime datos, el cual escribe por pantalla los valores de sus propiedades.
+Escribe una clase Producto para crear objetos. Estos objetos, deben presentar las propiedades código, nombre y precio, además del método imprime datos, 
+el cual escribe por pantalla los valores de sus propiedades.
 Posteriormente, cree tres instancias de este objeto y guárdalas en un array.
 Por último, utilice el método imprime datos para mostrar por pantalla los valores de los tres objetos instanciados.
 */
-
+let producto = {
+    codigo : "", 
+    nombre : "",
+    precio : "",
+    verProducto : function () {
+        return `El producto ${this.nombre}, código ${this.codigo}, tiene un precio de $${this.precio}`
+    },
+}
 
