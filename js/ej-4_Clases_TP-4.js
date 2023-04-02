d1bb2eddeafd57e47bdd57cc926bc2614d976cb8
Luciano Angeleri <lcnang@gmail.com>
2023-04-02 15:29:43 -0300

    Se agregó la clase Producto con los métodos correspondientes

--- js/ej-4_Clases_TP-4.js
+++ js/ej-4_Clases_TP-4.js
@@ -5,12 +5,37 @@ el cual escribe por pantalla los valores de sus propiedades.
 Posteriormente, cree tres instancias de este objeto y guárdalas en un array.
 Por último, utilice el método imprime datos para mostrar por pantalla los valores de los tres objetos instanciados.
 */
-let producto = {
-    codigo : "", 
-    nombre : "",
-    precio : "",
-    verProducto : function () {
-        return `El producto ${this.nombre}, código ${this.codigo}, tiene un precio de $${this.precio}`
-    },
+class Producto {
+    #codigo
+    #precio
+    constructor(codigo,nombre,precio){
+        this.#codigo = codigo 
+        this._nombre = nombre 
+        this.#precio = precio 
+    }
+    get codigo(){
+        return this.#codigo
+    }
+    set codigo(nuevoCodigo){
+        this.#codigo = nuevoCodigo
+    }
+    get precio(){
+        return this.#precio
+    }
+    set precio(nuevoPrecio){
+        if (this.precio >= 0 && !isNaN(this.precio)){
+            this.#precio = nuevoPrecio
+        }
+    }
+    verProducto() {
+        return `Código: ${this.codigo} - Nombre del Producto: ${this.nombre} - Precio: $${this.precio}`
+    }
+    verProductoComoLista() {
+        document.write('<ul class="list-group">')
+        document.write(`<li class="list-group-item">${this.codigo}</li>`)
+        document.write(`<li class="list-group-item">${this.nombre}</li>`)
+        document.write(`<li class="list-group-item">${this.precio}</li>`)
+        document.write('</ul>') 
+    }
 }
 
