0b09adbc8385d9294cb75a34405231673992d78a
Luciano Angeleri <lcnang@gmail.com>
2023-04-02 20:35:04 -0300

    Se crearon lo métodos restantes que pide el ejercicio y se lo resolvió

--- js/ej-5_Clases_TP-4.js
+++ js/ej-5_Clases_TP-4.js
@@ -10,103 +10,134 @@ mostrarDatos: devuelve toda la información del objeto.
 generaDNI(): genera un número aleatorio de 8 cifras.
 */
 class Persona {
-    #nombre
-    #edad
-    #dni
-    #sexo
-    _peso
-    _altura
-    #fechaNacimiento
-  constructor(nombre, edad, dni, sexo, peso, altura, fechaNacimiento) {
-    this.#nombre = nombre
-    this.#edad = edad
-    this.#dni = dni
+  #nombre;
+  #edad;
+  #dni;
+  #sexo;
+  _peso;
+  _altura;
+  #anioNacimiento;
+  constructor(nombre, edad, dni, sexo, peso, altura, anioNacimiento) {
+    this.#nombre = nombre;
+    this.#edad = edad;
+    this.#dni = dni;
     this.#sexo = sexo;
     this._peso = peso;
-    this._altura = altura
-    this.#fechaNacimiento = fechaNacimiento;
+    this._altura = altura;
+    this.#anioNacimiento = anioNacimiento;
   }
-  get nombre(){
+  get nombre() {
     return this.#nombre;
   }
-  set nombre(nuevoNombre){
-    if (nuevoNombre.length>0){
-        this.#nombre = nuevoNombre;
+  set nombre(nuevoNombre) {
+    if (nuevoNombre.length > 0) {
+      this.#nombre = nuevoNombre;
     }
   }
-  get edad(){
+  get edad() {
     return this.#edad;
   }
-  set edad(nuevaEdad){
-    if (nuevaEdad>0 && !isNaN(nuevaEdad)){
-        this.#edad = nuevaEdad;
+  set edad(nuevaEdad) {
+    if (nuevaEdad > 0 && !isNaN(nuevaEdad)) {
+      this.#edad = nuevaEdad;
     }
   }
-  get dni(){
+  get dni() {
     return this.#dni;
   }
-  set dni(nuevoDni){
-    if (nuevoDni>0 && !isNaN(nuevoDni)){
-        this.#dni = nuevoDni;
+  set dni(nuevoDni) {
+    if (nuevoDni > 0 && !isNaN(nuevoDni)) {
+      this.#dni = nuevoDni;
     }
   }
-  get sexo(){
+  get sexo() {
     return this.#sexo;
   }
-  set sexo(nuevoSexo){
-    if (nuevoSexo === "H" && nuevoSexo === "M"){
-        this.#sexo = nuevoSexo;
+  set sexo(nuevoSexo) {
+    if (nuevoSexo === "H" && nuevoSexo === "M") {
+      this.#sexo = nuevoSexo;
     }
   }
-  get peso(){
+  get peso() {
     return this._peso;
   }
-  set peso(nuevoPeso){
-    if (nuevoPeso>0){
-        this._peso = nuevoPeso;
+  set peso(nuevoPeso) {
+    if (nuevoPeso > 0) {
+      this._peso = nuevoPeso;
     }
   }
-  get altura(){
+  get altura() {
     return this._altura;
   }
-  set altura(nuevaAltura){
-    if (nuevaAltura>0){
-        this._altura = nuevaAltura;
+  set altura(nuevaAltura) {
+    if (nuevaAltura > 0) {
+      this._altura = nuevaAltura;
     }
   }
-  get fechaNacimiento(){
-    return this._fechaNacimiento;
+  get anioNacimiento() {
+    return this.#anioNacimiento;
   }
-  set fechaNacimiento(nuevaFechaNacimiento){
-    if (nuevaFechaNacimiento>0){
-        this._fechaNacimiento = nuevaFechaNacimiento;
+  set anioNacimiento(nuevoAnioNacimiento) {
+    if (nuevoAnioNacimiento > 0) {
+      this.#anioNacimiento = nuevoAnioNacimiento;
     }
   }
-  mostrarGeneracion(){
-    //ostrar un mensaje indicando a qué generación pertenece la persona creada y cual es el rasgo característico de esta generación
+  mostrarGeneracion() {
+    if (this.anioNacimiento >= 1994 && this.anioNacimiento <= 2010) {
+      return `${this.nombre} pertenece a la "Generación Z" cuyo rasgo es la "Irreverencia"`;
+    } else if (this.anioNacimiento >= 1981 && this.anioNacimiento <= 1993) {
+      return `${this.nombre} pertenece a la "Generación Y" cuyo rasgo es la "Frustración"`;
+    } else if (this.anioNacimiento >= 1969 && this.anioNacimiento <= 1980) {
+      return `${this.nombre} pertenece a la "Generación X" cuyo rasgo es la "Obsesión por el éxito"`;
+    } else if (this.anioNacimiento >= 1949 && this.anioNacimiento <= 1968) {
+      return `${this.nombre} pertenece a la "Baby Boom" cuyo rasgo es la "Ambición"`;
+    } else if (this.anioNacimiento >= 1930 && this.anioNacimiento <= 1948) {
+      return `${this.nombre} pertenece a la "Silent Generation" cuyo rasgo es la "Austeridad"`;
+    }
   }
-  esMayorDeEdad(){
-    return this.edad >=18 ? "Es mayor de edad" : "Es menor de edad"
+  esMayorDeEdad() {
+    return this.edad >= 18 ? `${this.nombre} es mayor de edad` : `${this.nombre} es menor de edad`;
   }
 
-  mostrarDatos(){
+  mostrarDatos() {
     document.write('<ul class="list-group py-3 fs-3">');
-    document.write(`<li class="list-group-item">Nombre de la persona: ${this.edad}</li>`);
-    document.write(`<li class="list-group-item">DNI de la persona: ${this.dni}</li>`);
-    document.write(`<li class="list-group-item">Sexo de la persona: ${this.sexo}</li>`);
-    document.write(`<li class="list-group-item">Peso de la persona: ${this.peso} kilogramos</li>`);
-    document.write(`<li class="list-group-item">Altura de la persona: ${this.altura} metros</li>`);
-    document.write(`<li class="list-group-item">Fecha de nacimiento de la persona: ${this.fechaNacimiento}</li>`);
+    document.write(`<li class="list-group-item">Nombre: ${this.nombre}</li>`);
+    document.write(`<li class="list-group-item">Edad: ${this.edad} años</li>`);
+    document.write(`<li class="list-group-item">DNI: ${this.dni}</li>`);
+    document.write(`<li class="list-group-item">Sexo: ${this.sexo === "H" ? "Hombre" : "Mujer"}</li>`);
+    document.write(`<li class="list-group-item">Peso: ${this.peso} kilogramos</li>`);
+    document.write(`<li class="list-group-item">Altura: ${this.altura} metros</li>`);
+    document.write(`<li class="list-group-item">Año de nacimiento: ${this.anioNacimiento}</li>`);
     document.write("</ul>");
   }
-  generaDNI(){
-    let dniAleatorio ="";
+  generaDNI() {
+    let dniAleatorio = "";
     for (let i = 0; i < 8; i++) {
-        let numeroAleatorio = Math.floor(Math.random() * 10); 
-        dniAleatorio += numeroAleatorio.toString()  
+      let numeroAleatorio = Math.floor(Math.random() * 10);
+      dniAleatorio += numeroAleatorio.toString();
     }
-    return dniAleatorio 
+    this.dni = dniAleatorio;
   }
 }
+let persona1 = new Persona("Luciano", 27, "39141056", "H", 96, 1.84, 1995);
 
+document.write('<section class="container mt-3">')
+document.write('<h5 class="display-5 text-success py-2">Uso el método mostrarDatos()</h5>');
+persona1.mostrarDatos();
+document.write('</section>');
 
+document.write('<section class="container mt-3">')
+document.write('<h5 class="display-5 text-success py-2">Uso el método mostrarGeneracion()</h5>');
+document.write(`<h6 class="display-6 text-secondary py-3">${persona1.mostrarGeneracion()}</h5>`);
+document.write('</section>');
+
+document.write('<section class="container mt-3">')
+document.write('<h5 class="display-5 text-primary py-2">Uso el método esMayorDeEdad()</h5>');
+document.write(`<h6 class="display-6 text-secondary py-3">${persona1.esMayorDeEdad()}</h5>`);
+document.write('</section>');
+
+document.write('<section class="container mt-3">')
+document.write('<h5 class="display-5 text-primary py-2">Uso el método generarDNI() para generar un DNI aleatorio en la persona</h5>');
+persona1.generaDNI()
+persona1.mostrarDatos();
+document.write('</section>');
