bc3643bbf23e7336ed3639d48e2c297c4dcfb938
Luciano Angeleri <lcnang@gmail.com>
2023-04-02 19:51:38 -0300

    Se crearon los métodos de get y set para las propiedades de la clase y otros métodos

--- js/ej-5_Clases_TP-4.js
+++ js/ej-5_Clases_TP-4.js
@@ -26,4 +26,87 @@ class Persona {
     this._altura = altura
     this.#fechaNacimiento = fechaNacimiento;
   }
+  get nombre(){
+    return this.#nombre;
+  }
+  set nombre(nuevoNombre){
+    if (nuevoNombre.length>0){
+        this.#nombre = nuevoNombre;
+    }
+  }
+  get edad(){
+    return this.#edad;
+  }
+  set edad(nuevaEdad){
+    if (nuevaEdad>0 && !isNaN(nuevaEdad)){
+        this.#edad = nuevaEdad;
+    }
+  }
+  get dni(){
+    return this.#dni;
+  }
+  set dni(nuevoDni){
+    if (nuevoDni>0 && !isNaN(nuevoDni)){
+        this.#dni = nuevoDni;
+    }
+  }
+  get sexo(){
+    return this.#sexo;
+  }
+  set sexo(nuevoSexo){
+    if (nuevoSexo === "H" && nuevoSexo === "M"){
+        this.#sexo = nuevoSexo;
+    }
+  }
+  get peso(){
+    return this._peso;
+  }
+  set peso(nuevoPeso){
+    if (nuevoPeso>0){
+        this._peso = nuevoPeso;
+    }
+  }
+  get altura(){
+    return this._altura;
+  }
+  set altura(nuevaAltura){
+    if (nuevaAltura>0){
+        this._altura = nuevaAltura;
+    }
+  }
+  get fechaNacimiento(){
+    return this._fechaNacimiento;
+  }
+  set fechaNacimiento(nuevaFechaNacimiento){
+    if (nuevaFechaNacimiento>0){
+        this._fechaNacimiento = nuevaFechaNacimiento;
+    }
+  }
+  mostrarGeneracion(){
+    //ostrar un mensaje indicando a qué generación pertenece la persona creada y cual es el rasgo característico de esta generación
+  }
+  esMayorDeEdad(){
+    return this.edad >=18 ? "Es mayor de edad" : "Es menor de edad"
+  }
+
+  mostrarDatos(){
+    document.write('<ul class="list-group py-3 fs-3">');
+    document.write(`<li class="list-group-item">Nombre de la persona: ${this.edad}</li>`);
+    document.write(`<li class="list-group-item">DNI de la persona: ${this.dni}</li>`);
+    document.write(`<li class="list-group-item">Sexo de la persona: ${this.sexo}</li>`);
+    document.write(`<li class="list-group-item">Peso de la persona: ${this.peso} kilogramos</li>`);
+    document.write(`<li class="list-group-item">Altura de la persona: ${this.altura} metros</li>`);
+    document.write(`<li class="list-group-item">Fecha de nacimiento de la persona: ${this.fechaNacimiento}</li>`);
+    document.write("</ul>");
+  }
+  generaDNI(){
+    let dniAleatorio ="";
+    for (let i = 0; i < 8; i++) {
+        let numeroAleatorio = Math.floor(Math.random() * 10); 
+        dniAleatorio += numeroAleatorio.toString()  
+    }
+    return dniAleatorio 
+  }
 }
+
+
