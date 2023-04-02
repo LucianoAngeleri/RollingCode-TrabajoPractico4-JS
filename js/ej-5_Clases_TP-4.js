62d0aaee3fd5564fe7738a60de7e838d362e9f92
Luciano Angeleri <lcnang@gmail.com>
2023-04-02 18:53:10 -0300

    Se aadió la clase Persona y su constructor

--- js/ej-5_Clases_TP-4.js
+++ js/ej-5_Clases_TP-4.js
@@ -2,10 +2,28 @@
 Ejercicio 5 - Generaciones - Modelando Clases
  Crea una clase llamada Persona que siga las siguientes condiciones:
 Sus propiedades son: nombre, edad, DNI, sexo (H hombre, M mujer), peso y altura, año de nacimiento. Si quieres añadir alguna propiedad extra puedes hacerlo.
-Los métodos que se debe poder utilizar  son:
+Los métodos que se debe poder utilizar son:
 mostrarGeneracion: este método debe mostrar un mensaje indicando a qué generación pertenece la persona creada y cual es el rasgo característico de esta generación.
 Para realizar este método tener en cuenta la tabla de generaciones del documento.
 esMayorDeEdad: indica si es mayor de edad, devuelve un mensaje indicando que la persona es mayor de edad.
 mostrarDatos: devuelve toda la información del objeto.
 generaDNI(): genera un número aleatorio de 8 cifras.
 */
+class Persona {
+    #nombre
+    #edad
+    #dni
+    #sexo
+    _peso
+    _altura
+    #fechaNacimiento
+  constructor(nombre, edad, dni, sexo, peso, altura, fechaNacimiento) {
+    this.#nombre = nombre
+    this.#edad = edad
+    this.#dni = dni
+    this.#sexo = sexo;
+    this._peso = peso;
+    this._altura = altura
+    this.#fechaNacimiento = fechaNacimiento;
+  }
+}
